2b6d17eca01bfdadba6f2dd3be0179842ce552a7
jsaraiya1 <saraiyajay@hotmail.com>
2017-08-09 20:11:56 -0500

    add source

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.allstate.jigsaw {
+    requires java.logging;
+   // requires commons.logging;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+    
+    requires  spring.core;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.aop;
+   // requires spring.web;
+    requires spring.expression;
+
+    requires spring.boot;
+    //requires spring.boot.autoconfigure;
+
+}
